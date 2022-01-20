b855afd6d4d45ecdce859f686056d8060448d558
Lucas Nabesima <lucas.nabesima@gmail.com>
2022-01-20 14:53:10 -0300

    Create a tooltip when hovering map in contact area

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initAccordion from './modules/accordion.js';
 import initSmoothScroll from './modules/smoothScroll.js';
 import initAnimateScroll from './modules/animateScroll.js';
 import initModal from './modules/modal.js'
+import initTooltip from './modules/tooltip.js'
 
 initTabMenu();
 initAccordion();
 initSmoothScroll();
 initAnimateScroll();
 initModal();
+initTooltip();
