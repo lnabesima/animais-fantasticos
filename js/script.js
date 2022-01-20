ca2c8e0d0cf30cd6c6bdec8db69114ac041776f2
Lucas Nabesima <lucas.nabesima@gmail.com>
2022-01-20 13:50:33 -0300

    Add modal functionality

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initTabMenu from './modules/tabMenu.js';
 import initAccordion from './modules/accordion.js';
 import initSmoothScroll from './modules/smoothScroll.js';
 import initAnimateScroll from './modules/animateScroll.js';
+import initModal from './modules/modal.js'
 
 initTabMenu();
 initAccordion();
 initSmoothScroll();
 initAnimateScroll();
+initModal();
