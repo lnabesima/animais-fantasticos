37da0e630ca786f8487e8dc7692608ad1c212f32
Lucas Nabesima <lucas.nabesima@gmail.com>
2022-02-07 09:11:28 -0300

    Refactor fetchAnimals to spread responsabilities

--- js/modules/fetchAnimals.js
+++ js/modules/fetchAnimals.js
@@ -1,32 +1,41 @@
-import NumberAnimation from './numberAnimation.js'
+import NumberAnimation from './numberAnimation.js';
 
-export default function initFetchAnimals() {
-  async function fetchAnimals(url) {
-    function createAnimal(animal) {
-      const div = document.createElement('div');
-      div.classList.add('numero-animal');
-      div.innerHTML = `<h3>${animal.species[0].toUpperCase()}${animal.species.substring(
-        1
-      )}</h3><span data-numero>${animal.total}</span>`;
-      return div;
-    }
+export default function fetchAnimals(url, target) {
+  const numerosGrid = document.querySelector(target);
 
+  function createAnimal(animal) {
+    const div = document.createElement('div');
+    div.classList.add('numero-animal');
+    div.innerHTML = `<h3>${animal.species[0].toUpperCase()}${animal.species.substring(
+      1
+    )}</h3><span data-numero>${animal.total}</span>`;
+    return div;
+  }
+
+  function fillAnimalData(animal) {
+    const divAnimal = createAnimal(animal);
+    numerosGrid.appendChild(divAnimal);
+  }
+
+  function animateAnimalNumbers() {
+    const numberAnimation = new NumberAnimation(
+      '[data-numero]',
+      '.numeros',
+      'active'
+    );
+    numberAnimation.init();
+  }
+
+  async function createAnimals() {
     try {
       const response = await fetch(url);
       const responseJSON = await response.json();
-      const numerosGrid = document.querySelector('.numeros-grid');
+      responseJSON.forEach(animal => fillAnimalData(animal));
 
-      responseJSON.forEach(animal => {
-        const divAnimal = createAnimal(animal);
-        numerosGrid.appendChild(divAnimal);
-      });
-      
-      const numberAnimation = new NumberAnimation('[data-numero]', '.numeros', 'active');
-      numberAnimation.init();
+      animateAnimalNumbers();
     } catch (error) {
       console.error(error);
     }
   }
-
-  fetchAnimals('./src/animaisApi.json');
+  return createAnimals();
 }
