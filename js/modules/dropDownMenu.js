89b1ac19ec04eff4b386536b50a5ce1d6fe87394
Lucas Nabesima <lucas.nabesima@gmail.com>
2022-02-07 17:07:38 -0300

    Refactor dropdownMenu function to class

--- js/modules/dropDownMenu.js
+++ js/modules/dropDownMenu.js
@@ -1,20 +1,39 @@
 import outsideClick from './outsideClick.js';
 
-export default function initDropDownMenu() {
-  const dropDownMenus = document.querySelectorAll('[data-dropdown]');
-  const events = ['click', 'touchstart'];
+export default class DropDownMenu {
+  constructor(dropdownMenus, classe, events) {
+    this.dropDownMenus = document.querySelectorAll(dropdownMenus);
+    this.classe = classe;
+    if (this.events === undefined) {
+      this.events = ['click', 'touchstart'];
+    } else {
+      this.events = events;
+    }
 
-  function handleClick(event) {
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
     event.preventDefault();
-    this.classList.add('active');
-    outsideClick(this, events, () => {
-      this.classList.remove('active');
+    event.currentTarget.classList.add(this.classe);
+    outsideClick(event.currentTarget, this.events, () => {
+      event.currentTarget.classList.remove(this.classe);
     });
   }
 
-  dropDownMenus.forEach(menu => {
-    ['click', 'touchstart'].forEach(userEvent => {
-      menu.addEventListener(userEvent, handleClick);
+  addDropdownMenuEvent() {
+    this.dropDownMenus.forEach(menu => {
+      this.events.forEach(userEvent => {
+        menu.addEventListener(userEvent, this.handleClick);
+      });
     });
-  });
+  }
+
+  init() {
+    if (this.dropDownMenus.lenght) {
+      this.addDropdownMenuEvent();
+    }
+
+    return this;
+  }
 }
