e551d3047de2fcefcb3e5ef317092f0ee2095319
Lucas Nabesima <lucas.nabesima@gmail.com>
2022-02-07 09:30:32 -0300

    Refactor fetchBitcoin function

--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@ import Accordion from './modules/accordion.js';
 import TabMenu from './modules/tabMenu.js';
 import Modal from './modules/modal.js';
 import Tooltip from './modules/tooltip.js';
+import fetchAnimals from './modules/fetchAnimals.js';
 
 import initAnimateScroll from './modules/animateScroll.js';
 import initDropDownMenu from './modules/dropDownMenu.js';
 import initMobileMenu from './modules/mobileMenu.js';
 import initWorkingHours from './modules/workingHours.js';
-import fetchAnimals from './modules/fetchAnimals.js';
-import initFetchBitcoinPrice from './modules/fetchBitcoinPrice.js';
+import fetchBitcoinPrice from './modules/fetchBitcoinPrice.js';
 
 const smoothScroll = new SmoothScroll('[data-menu] a[href^="#"]');
 smoothScroll.init();
@@ -30,9 +30,10 @@ modal.init();
 const tooltip = new Tooltip('[data-tooltip]');
 tooltip.init();
 
+fetchAnimals('./src/animaisApi.json', '.numeros-grid');
+fetchBitcoinPrice(`https://blockchain.info/ticker`, '.btc-price');
+
 initAnimateScroll();
 initDropDownMenu();
 initMobileMenu();
 initWorkingHours();
-fetchAnimals('./src/animaisApi.json', '.numeros-grid');
-initFetchBitcoinPrice();
